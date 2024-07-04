16a2c9a5003693f297a80567ac2fd09af12f2c83
Chiazor Daniel <chiazordaniel317@gmail.com>
2024-07-04 23:09:57 +0100

    fix

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import Hamburger from "./icons/hamburger";
-import Search from "./icons/search";
-import Favourite from "./icons/favourite";
-import ShoppingBag from "./icons/ShoppingBag";
+import { FaBars, FaSearch, FaHeart, FaShoppingBag } from "react-icons/fa";
 
 function Navbar() {
   return (
     <div className="flex w-full p-2 px-4 justify-between items-center">
       <div className="flex gap-3 items-center"> 
-        <Hamburger />
-        <Search />
+        <FaBars size={24} />
+        <FaSearch size={24} />
       </div>
       <h2 className="font-bold text-2xl">FARFETCH</h2>
       <div className="flex gap-3 items-center">
-        <Favourite />
-        <ShoppingBag />
+        <FaHeart size={24} />
+        <FaShoppingBag size={24} />
       </div>
     </div>
   );
